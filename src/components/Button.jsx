4040e04522b7abc5a11f468e2a8107039b8750f6
Namfon <varisa.vb@hotmail.com>
2024-06-04 13:17:35 +0700

    feat(App.jsx,Button.jsx): Add Button Component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,31 @@
 // Start coding here
+import { css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
 
-function Button() {}
+function Button(props) {
+  let color = "black";
+  switch (props.name) {
+    case "primary":
+      color = "#074ee8";
+      break;
+    case "secondary":
+      color = "#07A4E8";
+      break;
+  }
+  return (
+    <button
+      css={css`
+        width: 170px;
+        height: 50px;
+        color: white;
+        background-color: ${color};
+        border-radius: 4px;
+        margin: 8px;
+      `}
+    >
+      Button
+    </button>
+  );
+}
+
+export default Button;
