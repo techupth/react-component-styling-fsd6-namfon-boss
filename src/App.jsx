d85748e95ec60e2a83558223dbe362b58ea59426
paphonwat <paphonwat.w@gmail.com>
2024-06-06 01:54:50 +0700

    feat:sent React Component Styling Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
+import Button from "./components/Button.jsx";
+import Alert from "./components/Alert.jsx";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
+        <Button color="primary" text="Primary" />
+        <Button color="secondary" text="Secondary" />
       </div>
       <hr />
       <div className="alert-components-section">
         {/* Render ตัว Alert 4 แบบ */}
+        <Alert status="error" />
+        <Alert status="warning" />
+        <Alert status="info" />
+        <Alert status="success" />
       </div>
     </div>
   );
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,65 @@
 // Start coding here
+import { css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import errorImg from "./frown.svg";
+import warningImg from "./alert-triangle.svg";
+import infoImg from "./alert-circle.svg";
+import successImg from "./check-circle.svg";
 
-function Alert() {}
+function Alert(props) {
+    const status = {
+      error: {
+        icon: errorImg,
+        color: "#F9C8C8",
+      },
+      warning: {
+        icon: warningImg,
+        color: "#F9D9C8",
+      },
+      info: {
+        icon: infoImg,
+        color: "#F9EBC8",
+      },
+      success: {
+        icon: successImg,
+        color: "#CEF7CD",
+      },
+    };
+    return (
+      <div
+        css={css`
+          width: 300px;
+          height: 50px;
+          background-color: ${status[props.status].color};
+          border-radius: 10px;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          margin: 30px;
+          font-family: "Kanit", sans-serif;
+        `}
+      >
+        <img
+          css={css`
+            width: 24px;
+            height: 24px;
+            margin: 0px 20px;
+          `}
+          src={status[props.status].icon}
+        />
+        <p
+          css={css`
+            color: #444444;
+            font-weight: 400;
+            font-size: 20px;
+            padding: 0px;
+            margin: 0px;
+          `}
+        >
+          This is {props.status} alert box
+        </p>
+      </div>
+    );
+  }
+  
+  export default Alert;
